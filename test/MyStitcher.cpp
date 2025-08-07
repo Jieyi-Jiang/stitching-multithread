e5210b06f508119ca502ce812df8aa3de7c73053
Jieyi Jiang <jieyi_jiang@qq.com>
2025-08-07 21:48:58 +0800

    add the notes and MyStitcher class

---
+++ test/MyStitcher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jieyi on 25-8-7.
+//
+
+#include "MyStitcher.h"
---
+++ test/MyStitcher.h
@@ -0,0 +1,16 @@
+//
+// Created by jieyi on 25-8-7.
+//
+
+#ifndef MYSTITCHER_H
+#define MYSTITCHER_H
+
+
+
+class MyStitcher {
+
+};
+
+
+
+#endif //MYSTITCHER_H
